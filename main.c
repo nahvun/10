5ade96300064c047a9ec3a72b69fcfcf4eb43238
Nahyun Kim <nahyun@sookmyung.ac.kr>
2022-11-08 16:41:11 +0900

    10_01

--- main.c
+++ main.c
@@ -5,14 +5,12 @@
 
 int main(int argc, char *argv[]) {
 
-	int i = 0;
-	char str[4];
-	str[0] = 'a';
-	str[1] = 'b';
-	str[2] = 'c';
-	str[3] = '\0';
+	char code1 = 'A';
+	int code2 = 65 ;
 
-	printf("%s\n", str);
+	printf("code1 = %c, (%d)\n", code1, code1);
+	printf("code2 = %c, (%d)\n", code2, code2);
+
 
 	return 0;
 }
